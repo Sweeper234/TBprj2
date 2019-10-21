93c13fa7e03e342fa9a4942ca9a26a7cdb94f30b
Sweeper234 <56818928+Sweeper234@users.noreply.github.com>
2019-10-21 18:48:29 +1000

    Add files via upload

---
+++ CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public int distance;
+    public int angle;
+    public GameObject target;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.position = target.transform.position - transform.forward * distance;
+        transform.rotation = Quaternion.AngleAxis(angle, transform.right);
+    }
+}
